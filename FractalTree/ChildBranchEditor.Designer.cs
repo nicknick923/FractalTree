94cb16edff590545c4f8d5fae4298d61b048b6e6
Nick Sosinski <Nick.j.sosinski@hotmail.com>
2016-12-14 16:02:08 -0600

    Editing length works

--- FractalTree/ChildBranchEditor.Designer.cs
+++ FractalTree/ChildBranchEditor.Designer.cs
@@ -28,32 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.angleTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.recursionLengthTrackBar = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.angleTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recursionLengthTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 196);
+            this.panel1.Location = new System.Drawing.Point(12, 140);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(368, 228);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // angleTrackBar
+            // 
+            this.angleTrackBar.LargeChange = 10;
+            this.angleTrackBar.Location = new System.Drawing.Point(12, 89);
+            this.angleTrackBar.Maximum = 170;
+            this.angleTrackBar.Minimum = -170;
+            this.angleTrackBar.Name = "angleTrackBar";
+            this.angleTrackBar.Size = new System.Drawing.Size(368, 45);
+            this.angleTrackBar.TabIndex = 1;
+            this.angleTrackBar.TickFrequency = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "-170°";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "170°";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0°";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Angle";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Recursion Length";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(354, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "90%";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "10%";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // recursionLengthTrackBar
+            // 
+            this.recursionLengthTrackBar.LargeChange = 10;
+            this.recursionLengthTrackBar.Location = new System.Drawing.Point(12, 25);
+            this.recursionLengthTrackBar.Maximum = 90;
+            this.recursionLengthTrackBar.Minimum = 10;
+            this.recursionLengthTrackBar.Name = "recursionLengthTrackBar";
+            this.recursionLengthTrackBar.Size = new System.Drawing.Size(368, 45);
+            this.recursionLengthTrackBar.TabIndex = 6;
+            this.recursionLengthTrackBar.TickFrequency = 10;
+            this.recursionLengthTrackBar.Value = 10;
+            this.recursionLengthTrackBar.ValueChanged += new System.EventHandler(this.recursionLengthTrackBar_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // ChildBranchEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 436);
+            this.ClientSize = new System.Drawing.Size(392, 383);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.recursionLengthTrackBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.angleTrackBar);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "ChildBranchEditor";
             this.Text = "Branch Editor";
             this.Shown += new System.EventHandler(this.ChildBranchEditor_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.angleTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recursionLengthTrackBar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar angleTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TrackBar recursionLengthTrackBar;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- FractalTree/ChildBranchEditor.cs
+++ FractalTree/ChildBranchEditor.cs
@@ -13,23 +13,47 @@ namespace FractalTree
     public partial class ChildBranchEditor : Form
     {
         private Branch b;
-        private Graphics g;
+        private Graphics panelGraphics;
+        private Graphics bitmapGraphics;
+        private Bitmap drawingImage;
         public ChildBranchEditor(Branch inBranch)
         {
             InitializeComponent();
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
             b = inBranch;
-            g = panel1.CreateGraphics();
+            panelGraphics = panel1.CreateGraphics();
+
+            recursionLengthTrackBar.Value = (int)(b.getRecLength() * 100);
+            angleTrackBar.Value = b.getAngle();
         }
 
         private void ChildBranchEditor_Shown(object sender, EventArgs e)
         {
-            updateDrawing();
         }
 
         private void updateDrawing()
         {
-            g.Clear(Color.Gray);
-            b.DrawBranchInEditor(g);
+            drawingImage = new Bitmap(panel1.Size.Width, panel1.Size.Height);
+            bitmapGraphics = Graphics.FromImage(drawingImage);
+            //bitmapGraphics.Clear(Color.Gray);
+            b.DrawBranchInEditor(bitmapGraphics);
+            panelGraphics.DrawImage(drawingImage, 0, 0);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            updateDrawing();
+        }
+
+        private void recursionLengthTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            b = new Branch(b.getAngle(), (float)recursionLengthTrackBar.Value / 100);
+            panel1.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //updateDrawing();
         }
     }
 }
