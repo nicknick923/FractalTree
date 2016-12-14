f8f313015aa845bc4e3ce56967b854ad976216b7
Nick Sosinski <Nick.j.sosinski@hotmail.com>
2016-12-14 11:50:28 -0600

    Works now, About to add feature changing activies

--- FractalTree/Form1.Designer.cs
+++ FractalTree/Form1.Designer.cs
@@ -28,24 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.angleTrackBar = new System.Windows.Forms.TrackBar();
             this.lengthTrackBar = new System.Windows.Forms.TrackBar();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.recursionLengthTrackBar = new System.Windows.Forms.TrackBar();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.value1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.angleTrackBar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lengthTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recursionLengthTrackBar)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.AutoSize = true;
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
             this.tableLayoutPanel1.Controls.Add(this.angleTrackBar, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.lengthTrackBar, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.recursionLengthTrackBar, 2, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -59,29 +70,77 @@
             // 
             this.angleTrackBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.angleTrackBar.Location = new System.Drawing.Point(3, 356);
+            this.angleTrackBar.Maximum = 180;
             this.angleTrackBar.Name = "angleTrackBar";
-            this.angleTrackBar.Size = new System.Drawing.Size(341, 35);
+            this.angleTrackBar.Size = new System.Drawing.Size(198, 35);
             this.angleTrackBar.TabIndex = 0;
-            this.angleTrackBar.Scroll += new System.EventHandler(this.angleTrackBar_Scroll);
+            this.angleTrackBar.TickFrequency = 10;
+            this.angleTrackBar.Value = 45;
+            this.angleTrackBar.MouseHover += new System.EventHandler(this.angleTrackBar_MouseHover);
             // 
             // lengthTrackBar
             // 
             this.lengthTrackBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lengthTrackBar.Location = new System.Drawing.Point(350, 356);
+            this.lengthTrackBar.Location = new System.Drawing.Point(207, 356);
+            this.lengthTrackBar.Maximum = 250;
+            this.lengthTrackBar.Minimum = 20;
             this.lengthTrackBar.Name = "lengthTrackBar";
-            this.lengthTrackBar.Size = new System.Drawing.Size(342, 35);
+            this.lengthTrackBar.Size = new System.Drawing.Size(198, 35);
             this.lengthTrackBar.TabIndex = 1;
-            this.lengthTrackBar.Scroll += new System.EventHandler(this.lengthTrackBar_Scroll);
+            this.lengthTrackBar.TickFrequency = 25;
+            this.lengthTrackBar.Value = 100;
+            this.lengthTrackBar.MouseHover += new System.EventHandler(this.lengthTrackBar_MouseHover);
             // 
             // panel1
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 4);
+            this.panel1.ContextMenuStrip = this.contextMenuStrip1;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(689, 347);
             this.panel1.TabIndex = 2;
             this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(615, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // recursionLengthTrackBar
+            // 
+            this.recursionLengthTrackBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.recursionLengthTrackBar.Location = new System.Drawing.Point(411, 356);
+            this.recursionLengthTrackBar.Maximum = 100;
+            this.recursionLengthTrackBar.Minimum = 1;
+            this.recursionLengthTrackBar.Name = "recursionLengthTrackBar";
+            this.recursionLengthTrackBar.Size = new System.Drawing.Size(198, 35);
+            this.recursionLengthTrackBar.TabIndex = 4;
+            this.recursionLengthTrackBar.TickFrequency = 10;
+            this.recursionLengthTrackBar.Value = 67;
+            this.recursionLengthTrackBar.MouseHover += new System.EventHandler(this.recursionLengthTrackBar_MouseHover);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.value1ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // value1ToolStripMenuItem
+            // 
+            this.value1ToolStripMenuItem.Name = "value1ToolStripMenuItem";
+            this.value1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.value1ToolStripMenuItem.Text = "More Options";
+            this.value1ToolStripMenuItem.Click += new System.EventHandler(this.value1ToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -97,6 +156,8 @@
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.angleTrackBar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.lengthTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recursionLengthTrackBar)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +169,10 @@
         private System.Windows.Forms.TrackBar angleTrackBar;
         private System.Windows.Forms.TrackBar lengthTrackBar;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar recursionLengthTrackBar;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem value1ToolStripMenuItem;
     }
 }
 
--- FractalTree/Form1.cs
+++ FractalTree/Form1.cs
@@ -12,16 +12,27 @@ namespace FractalTree
 {
     public partial class Form1 : Form
     {
-
+        Bitmap bm;
         Graphics g;
         Pen drawingPen = Pens.Black;
         float gWidth;
         float gHeight;
-
+        BackgroundWorker drawer = new BackgroundWorker();
+        ToolTip t = new ToolTip();
 
         public Form1()
         {
             InitializeComponent();
+            drawer.WorkerSupportsCancellation = true;
+            drawer.WorkerReportsProgress = false;
+            drawer.DoWork += Drawer_DoWork;
+        }
+
+        private void Drawer_DoWork(object sender, DoWorkEventArgs e)
+        {
+            button1.Text = "Cancel";
+            DrawFractalTree(e);
+            button1.Text = "Draw";
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,54 +42,95 @@ namespace FractalTree
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-            DrawFractalTree();
+            button1_Click(null, null);
         }
 
-        private void angleTrackBar_Scroll(object sender, EventArgs e)
+        private void DrawFractalTree(DoWorkEventArgs e)
         {
-
-        }
-
-        private void lengthTrackBar_Scroll(object sender, EventArgs e)
-        {
-
-        }
-
-        private void DrawFractalTree()
-        {
-            g = panel1.CreateGraphics();
+            bm = new Bitmap(panel1.Size.Width, panel1.Size.Height);
+            //g = panel1.CreateGraphics();
+            g = Graphics.FromImage(bm);
+            gWidth = g.VisibleClipBounds.Width;
+            gHeight = g.VisibleClipBounds.Height;
             g.Clear(panel1.BackColor);
 
 
-            gWidth = g.VisibleClipBounds.Width;
-            gHeight = g.VisibleClipBounds.Height;
             g.TranslateTransform(gWidth / 2, gHeight);
-            Branch(200);
+            Branch(lengthTrackBar.Value, e);
             g.TranslateTransform(-(gWidth / 2), -(gHeight));
 
+            panel1.CreateGraphics().DrawImage(bm, 0,0);
+
         }
+
+
 
         private void panel1_Click(object sender, EventArgs e)
         {
-            DrawFractalTree();
         }
 
-        private void Branch(float length)
+        private void Branch(float length, DoWorkEventArgs e)
         {
-            g.DrawLine(drawingPen, 0, 0, 0, -length);
-            g.TranslateTransform(0, -length);
-            if (length > 4)
+            if (drawer.CancellationPending)
             {
-                g.RotateTransform(45);
-                Branch(length * .75F);
-                g.RotateTransform(-45);
-                g.RotateTransform(-45);
-                Branch(length * .75F);
-                g.RotateTransform(45);
+                e.Cancel = true;
             }
-            g.TranslateTransform(0, length);
+            else
+            {
+                g.DrawLine(drawingPen, 0, 0, 0, -length);
+                g.TranslateTransform(0, -length);
+                if (length > 4)
+                {
+                    g.RotateTransform(angleTrackBar.Value);
+                    Branch(length * recursionLengthTrackBar.Value / 100, e);
+                    g.RotateTransform(-angleTrackBar.Value);
+                    g.RotateTransform(.15F*-angleTrackBar.Value);
+                    Branch(length * recursionLengthTrackBar.Value / 100, e);
+                    g.RotateTransform(.15F*angleTrackBar.Value);
+                }
+                g.TranslateTransform(0, length);
+            }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!drawer.IsBusy)
+                drawer.RunWorkerAsync();
+            else
+                drawer.CancelAsync();
+        }
 
+        private void angleTrackBar_MouseHover(object sender, EventArgs e)
+        {
+            t.Hide(this);
+            t.Show("Use this slider to set the angle that the branches should extend in.", this, angleTrackBar.Location.X + 20, angleTrackBar.Location.Y, 2500);
+        }
+
+        private void lengthTrackBar_MouseHover(object sender, EventArgs e)
+        {
+            t.Hide(this);
+            t.Show("Use this slider to set the length of the trunk.", this, lengthTrackBar.Location.X + 20, lengthTrackBar.Location.Y, 2500);
+        }
+
+        private void recursionLengthTrackBar_MouseHover(object sender, EventArgs e)
+        {
+            t.Hide(this);
+            t.Show("Use this slider to set the recursiveness of the branches.", this, recursionLengthTrackBar.Location.X + 20, recursionLengthTrackBar.Location.Y, 2500);
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void value1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
